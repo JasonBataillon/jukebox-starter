ab329d0c4a71ddb00ac203d7c4fb193a60d6ab50
Your Name <jasonbataillon@gmail.com>
2024-10-16 07:53:30 -0400

    Finished createing everything.

---
+++ prisma/migrations/20241016111800_init/migration.sql
@@ -0,0 +1,32 @@
+/*
+  Warnings:
+
+  - You are about to drop the `_PlaylistToTrack` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "_PlaylistToTrack" DROP CONSTRAINT "_PlaylistToTrack_A_fkey";
+
+-- DropForeignKey
+ALTER TABLE "_PlaylistToTrack" DROP CONSTRAINT "_PlaylistToTrack_B_fkey";
+
+-- DropTable
+DROP TABLE "_PlaylistToTrack";
+
+-- CreateTable
+CREATE TABLE "_PlaylistTracks" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_PlaylistTracks_AB_unique" ON "_PlaylistTracks"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_PlaylistTracks_B_index" ON "_PlaylistTracks"("B");
+
+-- AddForeignKey
+ALTER TABLE "_PlaylistTracks" ADD CONSTRAINT "_PlaylistTracks_A_fkey" FOREIGN KEY ("A") REFERENCES "Playlist"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_PlaylistTracks" ADD CONSTRAINT "_PlaylistTracks_B_fkey" FOREIGN KEY ("B") REFERENCES "Track"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- prisma/seed.js
+++ prisma/seed.js
@@ -1,29 +1,44 @@
 const prisma = require('../prisma');
+const numUser = 5 + Math.floor(Math.random() * 5);
+const numPlaylist = 10 + Math.floor(Math.random() * 10);
+const numTrack = 20 + Math.floor(Math.random() * 20);
 
-const seed = async (numUsers = 5, numPlaylists = 10, numTracks = 20) => {
+const seed = async (
+  numUsers = numUser,
+  numPlaylists = numPlaylist,
+  numTracks = numTrack
+) => {
   const users = Array.from({ length: numUsers }, (_, i) => ({
-    username: `User ${i + 1}`,
+    username: `User ${Math.floor(Math.random() * 100)}`,
   }));
   await prisma.user.createManyAndReturn({ data: users });
 
-  const tracks = Array.from({ length: numtracks }, (_, j) => ({
+  const tracks = Array.from({ length: numTracks }, (_, j) => ({
     name: `Track ${j + 1}`,
   }));
   await prisma.track.createMany({ data: tracks });
 
+  const playlists = Array.from({ length: numPlaylists }, (_, l) => {
+    const ownerId = 1 + Math.floor(Math.random() * numUsers);
+    return {
+      name: `Playlist ${l + 1}`,
+      description: `Anything for ${l + 1}.`,
+      ownerId,
+    };
+  });
+  await prisma.playlist.createMany({ data: playlists });
+
   for (let i = 0; i < numPlaylists; i++) {
     const tracksTotal = 20 + Math.floor(Math.random() * numTracks);
 
-    const track = Array.from({ length: tracksTotal }, () => ({
-      id: 20 + Math.floor(Math.random() * numTracks),
+    const trackIds = Array.from({ length: tracksTotal }, () => ({
+      id: 1 + Math.floor(Math.random() * numTracks),
     }));
 
-    await prisma.playlist.create({
+    await prisma.playlist.update({
+      where: { id: i + 1 },
       data: {
-        name: `Playlist ${track}`,
-        description: `This is the ${ownerId}`,
-        ownerId: 5 + Math.floor(Math.random() * numUsers),
-        track: { connect: track },
+        tracks: { connect: trackIds.map((track) => ({ id: track.id })) },
       },
     });
   }
